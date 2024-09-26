0064fa407fb64cc83e5136078b4685986bed9020
Tarik <tarik.jaidani@gmail.com>
2024-09-27 00:17:05 +0200

    delete put

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -68,7 +68,15 @@ const updateContact = asyncHandler(async(req,res)=>{
 //@route PUT /api/contacts
 //@acces public
 const deleteContact = asyncHandler(async(req,res)=>{
-    res.status(200).json({message :`Delete contact for ${req.params.id}`})
+
+    const contact = await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404)
+        throw new Error('Contact not found ! ')
+    }
+    await Contact.deleteOne()
+       
+     res.status(200).json({message :`${contact} removed`})
 })
 
 module.exports = {getContact , getContacts, createContact, updateContact, deleteContact}
