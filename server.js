97c0cf1316a6c9f218d1c9e8798e72e5ee422bb8
Tarik <tarik.jaidani@gmail.com>
2024-09-26 18:05:40 +0200

    Express Router & Contacts CRUD Route Setup

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
-require('dotenv').config()
+require('dotenv').config();
+
+ 
+
 const app = express()
 
 const PORT = process.env.PORT
 
-app.get('/api/contacts', (req,res)=>{
-    res.status(200).json({message :"Get all Contacts"})
-})
+
+//app.use() enregistre le middleware pour gérer les requêtes vers /api/contacts,
+// et les route vers le fichier contactRoutes.
+app.use('/api/contacts', require("./routes/contactRoutes"))
 
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
