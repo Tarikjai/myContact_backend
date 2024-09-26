9335f4be3cf8e4ed2d459d3e0f1b3670dddb24ff
Tarik <tarik.jaidani@gmail.com>
2024-09-27 00:01:25 +0200

    CRUD API_GET_POST

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHanldler');
+const { connect } = require('./routes/contactRoutes');
+const connectDb = require('./config/dbConnection');
 require('dotenv').config();
 const app = express()
 const PORT = process.env.PORT
  
-
-
+connectDb()
 app.use(express.json())
-
-
-
-
 
 //app.use() enregistre le middleware pour gérer les requêtes vers /api/contacts,
 // et les route vers le fichier contactRoutes.
 app.use('/api/contacts', require("./routes/contactRoutes"))
 app.use(errorHandler)
-
 
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
